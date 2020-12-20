d89c10c24408ebe428d4b60fea4bc34faea918a8
Tom Golden <mail@tbjgolden.com>
2020-12-20 13:27:26 -0600

    try a more manual initial process

--- install.fish
+++ install.fish
@@ -2,4 +2,6 @@ mkdir -p ~/.setup
 cd ~/.setup
 git clone https://github.com/tbjgolden/unix-dev-setup.git
 cd ~/.setup/unix-dev-setup
-fish -i sync.fish
+
+echo "Step 1) Run `fish` to open a fish shell."
+echo "Step 2) Run `fish ~/.setup/unix-dev-setup/sync.fish` to start the install"
