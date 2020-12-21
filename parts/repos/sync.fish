556c472f0694f3d83d5b02508ff22a43d46f176d
Tom Golden <github@tbjgolden.com>
2020-12-21 12:56:32 -0600

    set github logic up to not crash with bad token

--- parts/repos/sync.fish
+++ parts/repos/sync.fish
@@ -3,42 +3,54 @@
 
 mkdir -p ~/Documents
 
-if test -z $GITHUB_USERNAME
-    set -U GITHUB_USERNAME (string trim (read -P 'github username'\n'» '))
+function set_github_credentials
+    if test -z $GITHUB_USERNAME
+        set -U GITHUB_USERNAME (string trim (read -P 'github username'\n'» '))
+    end
+
+    if test -z $GITHUB_TOKEN
+        set -U GITHUB_TOKEN (string trim (read -s -P 'github access token'\n'» '))
+    end
+
+    set ISAUTHED (curl -s -u $GITHUB_USERNAME':'$GITHUB_TOKEN "https://api.github.com/user" | jq '.total_private_repos')
 end
 
-if test -z $GITHUB_TOKEN
-    set -U GITHUB_TOKEN (string trim (read -s -P 'github access token'\n'» '))
+set ISAUTHED (curl -s -u $GITHUB_USERNAME':'$GITHUB_TOKEN "https://api.github.com/user" | jq '.total_private_repos')
+set_github_credentials
+while test $ISAUTHED = "null"
+    set_github_credentials
 end
 
+# Sets git email to github primary
 if test -z $GITHUB_EMAIL
-    set -U GITHUB_EMAIL (string trim (read -P 'github email'\n'» '))
+    set -U GITHUB_EMAIL (curl -s -u $GITHUB_USERNAME':'$GITHUB_TOKEN "https://api.github.com/user/emails" | jq -r ".[] | select(.primary == true) | .email")
 end
 
+# Sets git name to github name
 if test -z $GITHUB_NAME
     set -U GITHUB_NAME (curl -u $GITHUB_USERNAME':'$GITHUB_TOKEN "https://api.github.com/user" | jq ".name")
 end
 
+# Updates git credentials to include access token
 rm -f ~/.git-credentials
 echo -n "https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com" > ~/.git-credentials
 
+# Updates configs with updated data
 git config --global user.name $GITHUB_NAME
 git config --global user.email $GITHUB_EMAIL
 git config --global credential.helper store
 git config --global pull.rebase false
 git config --global init.defaultBranch main
 
-set REPOS_JSON ~/Documents/.repos.json
-
-rm -f $REPOS_JSON
-
-# fetches all repositories from the last 2 calendar years
-curl -u $GITHUB_USERNAME':'$GITHUB_TOKEN "https://api.github.com/user/repos?per_page=100&since="(echo (math (date +"%Y") - 2)'-01-01T00:00:01Z') | jq 'map({name,clone_url,private,fork})' >$REPOS_JSON
+# sync all repositories from the last 2 calendar years
+rm -f ~/Documents/.repos.json
+curl -s -u $GITHUB_USERNAME':'$GITHUB_TOKEN "https://api.github.com/user/repos?per_page=100&since="(echo (math (date +"%Y") - 2)'-01-01T00:00:01Z') | jq 'map({name,clone_url,private,fork})' >~/Documents/.repos.json
 
 # filter out forks and split them into open and closed source
 eval set PUBLIC_REPOS (echo (cat ~/Documents/.repos.json | jq -r 'map(select(.fork == false) | select(.private == false) | .clone_url) | @sh'))
 eval set PRIVATE_REPOS (echo (cat ~/Documents/.repos.json | jq -r 'map(select(.fork == false) | select(.private == true) | .clone_url) | @sh'))
 
+# clones any uncloned repos
 mkdir -p ~/Documents/open
 for PUBLIC_REPO in $PUBLIC_REPOS
   set -l frags (string split / $PUBLIC_REPO)
@@ -52,7 +64,6 @@ for PUBLIC_REPO in $PUBLIC_REPOS
     end
   end
 end
-
 mkdir -p ~/Documents/closed
 for PRIVATE_REPO in $PRIVATE_REPOS
   set -l frags (string split / $PRIVATE_REPO)
